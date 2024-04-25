4a86a3731adebe1c349958470517ccc1070ecf1b
trinhhoang1601 <86735744+trinhhoang1601@users.noreply.github.com>
2024-04-26 01:03:06 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Bae à <3',
+    introDesc: `Chuyện bắt đầu vào một ngày nọ, khi anh vẫn sống ngày tháng tươi đẹp. Phận sao phận bạc như vôi, khăng khăng bủa mãi lấy người hồng nhan. Vì em xinh đẹp quá, cho nên đời nó không cho anh cuộc sống dễ dàng. Mỗi ngày tương tư đến em, anh cũng muốn tỏ tình lắm. Nhưng mà, bước đi ba bước lại ngừng, thôi đành để vậy cầm chừng đợi nhau. Nhớ em đêm nhớ đêm nhung, nhưng nhớ mà trách nhưng sầu mà hư. Anh chợt nhận ra khí trời có vị ngọt, nước có màu hồng. À không, đấy là tình yêu màu hồng anh dành cho em đấy :>. Gửi Thủy <3`,
+    btnIntro: 'Hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <3',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰 Yêu em rứt nhiều  😘😘',
+    messDesc: 'Alo là có mặt luôn nè :>',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
